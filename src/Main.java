e5174f05509eec2343ed5758606d9de8787bf79a
LeM <mihail.lebedev88@gmail.com>
2023-02-28 21:08:06 +0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Уважаемый клиент");
+
+        int amount = 13676;
+        int miles = 20;
+        int bonus = amount / miles;
+
+        System.out.println("Количество Ваших премиуальных бонусов: " + bonus);
+
+        System.out.println("С наилучшими пожеланиями, АК Нетология");
+    }
+}
